94d53e6c2d98dde8ac641ff72b9b749760789308
Quinton <Quinton@lawn-143-215-95-65.lawn.gatech.edu>
2015-11-08 12:07:07 -0500

    Adding givens and householder

--- lu_fact.java
+++ lu_fact.java
@@ -2,13 +2,12 @@
 * @author Erik Maday, CG Carson, Quinton Johnson
 * @version 1.0
 */
-public class lu_fact{
+public class lu_fact {
 
   Matrix A;
 
-
   /**
-  * Calculates and returns hte calculated L matrix for LU Decomposition.
+  * Calculates and returns the calculated L matrix for LU Decomposition.
   * @return the calculated L matrix
   */
   public double[][] LDecomposition() {
---
+++ qr_fact_househ.java
@@ -0,0 +1,10 @@
+/**
+* @author Erik Maday, CG Carson, Quinton Johnson
+* @version 1.0
+*/
+public class qr_fact_househ {
+
+  Matrix A;
+
+
+}

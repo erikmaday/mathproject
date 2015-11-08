477fb5b8498dfcd4f93cbfd069c53829517add1d
Quinton <Quinton@lawn-143-215-95-65.lawn.gatech.edu>
2015-11-08 12:04:05 -0500

    Creating LU Decomposition

---
+++ lu_fact.java
@@ -0,0 +1,26 @@
+/**
+* @author Erik Maday, CG Carson, Quinton Johnson
+* @version 1.0
+*/
+public class lu_fact{
+
+  Matrix A;
+
+
+  /**
+  * Calculates and returns hte calculated L matrix for LU Decomposition.
+  * @return the calculated L matrix
+  */
+  public double[][] LDecomposition() {
+
+  }
+
+  public double[][] UDecomposition() {
+
+  }
+
+  public double LUerror() {
+
+  }
+
+}
